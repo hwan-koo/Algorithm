b8bea4116169dabc7c55d6d94416f77ecdc9c48c
경환 <110803716+hwan-koo@users.noreply.github.com>
2023-01-28 22:26:49 +0900

    [Silver V] Title: 단어 정렬, Time: 800 ms, Memory: 35168 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,11 @@
+N = int(input())
+word_list = []
+for _ in range(N):
+    word_list.append(input())
+
+word_list = list(set(word_list))
+word_list.sort()
+word_list.sort(key=lambda x: len(x))
+
+for i in word_list:
+    print(i)
