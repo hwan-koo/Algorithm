fe25822b9966d913c877bb0569040cd0251d5660
경환 <110803716+hwan-koo@users.noreply.github.com>
2023-01-31 16:09:52 +0900

    [Bronze II] Title: 블랙잭, Time: 84 ms, Memory: 37628 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -7,7 +7,7 @@ for i in range(N-2):
     for j in range(i+1, N-1):
         for k in range(j+1, N):
             sum_num = num_list[i] + num_list[j] + num_list[k]
-            sum_list.append(sum_num)
+            if sum_num <= M:
+                sum_list.append(sum_num)
 
-filter_sum_list = [n for n in sum_list if n <= M]
-print(max(filter_sum_list))
+print(max(sum_list))
