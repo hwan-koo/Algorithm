d7750f91347b1c245c19a1e90a9999ced3a89d02
경환 <110803716+hwan-koo@users.noreply.github.com>
2023-01-31 16:08:02 +0900

    [Bronze II] Title: 블랙잭, Time: 100 ms, Memory: 43936 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,15 @@
+import sys
+N, M = map(int, input().split())
+sys.setrecursionlimit(10 ** 6)
+
+num_list = list(map(int, input().split()))
+sum_list = []
+for i in range(N-2):
+    for j in range(i+1, N-1):
+        for k in range(j+1, N):
+            sum_num = num_list[i] + num_list[j] + num_list[k]
+            sum_list.append(sum_num)
+
+sum_list = list(set(sum_list))
+filter_sum_list = [n for n in sum_list if n <= M]
+print(max(filter_sum_list))
