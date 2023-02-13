92903ab28e96b03aba24d4d2ff889028b7fe8db4
경환 <110803716+hwan-koo@users.noreply.github.com>
2023-02-13 17:32:22 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -6,13 +6,11 @@ A = [[0 for _ in range(len(S)+1)] for _ in range(26)]
 for i in range(26):
     hb = 97 + i
     alphabet = chr(hb)
-    temp = 0
     for j in range(1, len(S)+1):
         if S[j-1] == alphabet:
-            temp += 1
-            A[i][j] = temp
+            A[i][j] = A[i][j-1] + 1
         else:
-            A[i][j] = temp
+            A[i][j] = A[i][j-1]
 
 for _ in range(q):
     alpha, l, r = map(str, input().split())
