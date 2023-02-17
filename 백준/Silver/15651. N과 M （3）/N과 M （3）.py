fe37562424b494fced870df6783d67c30bc511c6
경환 <110803716+hwan-koo@users.noreply.github.com>
2023-02-17 21:06:08 +0900

    [Silver III] Title: N과 M (3), Time: 2012 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,13 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+N, M = map(int, input().split())
+A = []
+def DFS():
+    if len(A) == M:
+        print(*A)
+        return
+    for i in range(1, N+1):
+        A.append(i)
+        DFS()
+        A.pop()
+DFS()
