b24d6f0fd780fd3075df81ef3780d2fbf52b58db
경환 <110803716+hwan-koo@users.noreply.github.com>
2023-02-21 17:45:56 +0900

    [Silver I] Title: IOIOI, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,15 +1,9 @@
 N = int(input())
 M = int(input())
 S = input()
-PN = ["I", "O"] * N
-PN.append("I")
+PN = "IO" * N + "I"
 count = 0
-temp = list(S[:2 * N + 1])
-if temp == PN:
-    count += 1
-for i in range(2 * N + 1, M):
-    temp.pop(0)
-    temp.append(S[i])
-    if temp == PN:
+for i in range(M-len(PN)+1):
+    if S[i:i+len(PN)] == PN:
         count += 1
 print(count)
