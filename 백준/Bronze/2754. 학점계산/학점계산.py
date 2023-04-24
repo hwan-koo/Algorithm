f90695d0e2505e9b5909313493804e29cc728acd
경환 <110803716+hwan-koo@users.noreply.github.com>
2023-04-24 18:25:04 +0900

    [Bronze V] Title: 학점계산, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,15 @@
+dic = {}
+dic["A+"] = 4.3
+dic["A0"] = 4.0
+dic["A-"] = 3.7
+dic["B+"] = 3.3
+dic["B0"] = 3.0
+dic["B-"] = 2.7
+dic["C+"] = 2.3
+dic["C0"] = 2.0
+dic["C-"] = 1.7
+dic["D+"] = 1.3
+dic["D0"] = 1.0
+dic["D-"] = 0.7
+dic["F"] = 0.0
+print(dic[input()])
